2a194f6eb23a263dd4f3c2579982df40cc2a53d0
Brian Barrow <briancbarrow@gmail.com>
2017-09-05 16:25:20 -0600

    Performing Client-Side Validation

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -9,13 +9,12 @@ namespace ModelValidation.Models
     public class Appointment
     {
         [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Please enter a date")]
         public DateTime Date { get; set; }
-
-        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+        
         public bool TermsAccepted { get; set; }
     }
 }
